99023bcb25163c159f2ef1ce203c6ef53b4f8ff4
veveLP <lukasekhouzar@seznam.cz>
2022-05-10 09:16:22 +0200

    v1

--- TestList/Form1.Designer.cs
+++ TestList/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(36, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(773, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(773, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(14, 178);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(377, 251);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(409, 178);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(379, 251);
+            this.listBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Test List";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
     }
 }
 
--- TestList/Form1.cs
+++ TestList/Form1.cs
@@ -16,5 +16,68 @@ namespace TestList
         {
             InitializeComponent();
         }
+        
+        public void vypis(List<String> list, ListBox listBox)
+        {
+            listBox.Items.AddRange(list.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            textBox1.Text = "";
+            Random random = new Random();
+            String txt = String.Empty;
+            for (int i = 0;i<(int)numericUpDown1.Value;i++)
+            {
+                txt += ((char)random.Next(32, 127)).ToString();
+            }
+            textBox1.Text = txt.Replace(" ", "");
+            List<String> slova = new List<string>();
+            List<String> cisla = new List<string>();
+            List<String> ostatni = new List<string>();
+            String temp = String.Empty;
+            foreach (char c in txt.Replace(" ", ""))
+            {
+                if(Char.IsLetter(c)) { temp += c; continue; }
+                if(temp.Length > 1) { slova.Add(temp); temp = ""; }
+                if (Char.IsDigit(c)) { cisla.Add(c.ToString()); continue; }
+                if(!(Char.IsLetterOrDigit(c))) { ostatni.Add(c.ToString()); continue; }
+            }
+            foreach (String str in txt.Split(' '))
+            {
+                //if(str.All(Char.IsLetter) && str.Length > 1) { slova.Add(str); continue; }
+                //if(str.All(Char.IsDigit) && str.Length > 0) { cisla.Add(str); MessageBox.Show(str); continue; }
+                //if(!str.All(Char.IsLetterOrDigit)) { ostatni.Add(str); }
+            }
+            cisla.Sort();
+            ostatni.Sort();
+            //ostatni.OrderBy(s => s, StringComparer.Ordinal);
+            /*bool sorted = false;
+            bool sorted2 = false;
+            while (sorted)
+            {
+                sorted = sorted2;
+                sorted2 = true;
+                for (int i = 0;i<ostatni.Count-1;i++)
+                {
+                    if((int)ostatni[i].ToCharArray()[0] > (int)ostatni[i+1].ToCharArray()[0])
+                    {
+                        char tmp = ostatni[i].ToCharArray()[0];
+                        ostatni[i].ToCharArray()[0] = ostatni[i+1].ToCharArray()[0];
+                        ostatni[i + 1].ToCharArray()[0] = tmp;
+                        sorted2 = false;
+                    }
+                }
+            }*/
+            vypis(slova, listBox1);
+            vypis(cisla, listBox2);
+            vypis(ostatni, listBox3);
+            if(cisla.Count %2 == 0) { MessageBox.Show("Medián = " + ((Double.Parse(cisla[cisla.Count / 2]) + Double.Parse(cisla[(cisla.Count/2)-1]))/2).ToString()); return; }
+            MessageBox.Show("Medián = "+cisla[cisla.Count / 2]);
+            
+        }
     }
 }
